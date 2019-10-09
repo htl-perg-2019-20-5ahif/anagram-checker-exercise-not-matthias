368182a0c85f6e96fb545e712c31fd7a5a92b3ae
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-10-09 07:47:21 +0200

    Added empty constructor so I can pass it via the body

--- anagram-checker/library/Anagram.cs
+++ anagram-checker/library/Anagram.cs
@@ -5,6 +5,11 @@
         public string W1 { get; set; }
         public string W2 { get; set; }
 
+        public Anagram()
+        {
+                
+        }
+
         public Anagram(string w1, string w2)
         {
             W1 = w1;
